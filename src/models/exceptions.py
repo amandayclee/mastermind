ee54ccce89916af55fedefcee36e14a11c061695
amandayclee <yician.lee@gmail.com>
2024-12-08 14:24:23 -0700

    git add api generator exceoption

--- src/models/exceptions.py
+++ src/models/exceptions.py
@@ -15,3 +15,15 @@ class RangeError(GuessError):
     def __init__(self):
         message = "Numbers must be between 0 and 7"
         super().__init__(message)
+
+class GeneratorError(Exception):
+    """code pattern generation not success"""
+    def __init__(self, message="Generator error occurred"):
+        self.message = message
+        super().__init__(self.message)
+
+class GameInitError(Exception):
+    """game initialization not success"""
+    def __init__(self, message="Failed to initialize game"):
+        self.message = message
+        super().__init__(self.message)
