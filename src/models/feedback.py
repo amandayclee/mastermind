c1f03f2dcad9aee2df3234adc6c7dba99ed83f7e
amandayclee <yician.lee@gmail.com>
2024-12-11 20:06:34 -0700

    add comment for feedback

--- src/models/feedback.py
+++ src/models/feedback.py
@@ -1,17 +1,41 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 class Feedback:
+    """
+    A class to represent feedback for a guess in mastermind in each round.
+    """
     def __init__(self, numbers_correct: int, positions_correct: int):
         """
-        initialize a feedback for a guess
+        Initialize a new Feedback instance.
+        
+        Attributes:
+            numbers_correct (int): Number of correct digits regardless of position
+            positions_correct (int): Number of digits that are both correct and in the correct position
         """
         self.numbers_correct = numbers_correct
         self.positions_correct = positions_correct
+        
+        logger.debug(f"Created new Feedback instance: {self}")
     
     def is_winning_guess(self, code_length: int) -> bool:
         """
-        check if the guess is right for both numbers and positions
+        Determine if the current feedback represents a winning guess, where it get the number of correct locations equals to the length of  the code pattern.
+        
+        Args:
+            code_length (int): The length of the code pattern
+            
+        Returns:
+            bool: True if this feedback represents a winning guess, False otherwise
         """
         return self.positions_correct == code_length
     
     def __str__(self) -> str:
-        """for display"""
+        """
+        Create a string representation of the feedback with desired display format.
+        
+        Returns:
+            str: A formatted string describing the feedback
+        """
         return f"{self.numbers_correct} correct numbers and {self.positions_correct} correct positions"
